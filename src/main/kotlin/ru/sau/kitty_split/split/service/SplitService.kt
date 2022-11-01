f9c2158da1228e3e74c43bedbba81e25762b4d0e
Pavel <psmas.pm@gmail.com>
2022-11-01 18:28:36 +0400

    WIP SplitService implementation

--- src/main/kotlin/ru/sau/kitty_split/split/service/SplitService.kt
+++ src/main/kotlin/ru/sau/kitty_split/split/service/SplitService.kt
@@ -1,6 +1,7 @@
 package ru.sau.kitty_split.split.service
 
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
 interface SplitService {
     fun splitBill(inputData: InputData): OutputData
@@ -9,6 +10,42 @@ interface SplitService {
 @Service
 class SplitServiceStub : SplitService {
     override fun splitBill(inputData: InputData): OutputData {
-        TODO("Not yet implemented")
+        val participants = inputData.participants
+        val spendings = inputData.spendings
+
+        val debt: MutableMap<String, MutableList<BigDecimal>> = mutableMapOf()
+
+        participants.forEach { participant ->
+            debt[participant] = spendings.asSequence().filter { spending ->
+                spending.payer == participant
+            }.map { spending -> spending.parts }.toList()[0].toMutableList()
+        }
+
+        for (spending in spendings) {
+            participants.forEach { participant ->
+                for (i in 0..participants.size) {
+                    debt[participant]!![participants.indexOf(participant)] -= spending.parts[i]
+                }
+            }
+        }
+        debt.filter { (participant, spending) -> !sum(spending).equals(0) }
+
+        val transactions = simpleTransactions(debt)
+
+        return OutputData(transactions.keys.toList(), transactions)
+    }
+
+    private fun sum(spendings: List<BigDecimal>): BigDecimal {
+        var sum = BigDecimal(0)
+        spendings.forEach { spending -> sum += spending }
+        return sum
+    }
+
+    private fun simpleTransactions(debt: MutableMap<String, MutableList<BigDecimal>>): Map<String, Map<String, BigDecimal>> {
+        val myMap: Map<String, Map<String, BigDecimal>> = mutableMapOf()
+
+        val paymentList = debt.map{ (participant, values) ->  Pair(participant,sum(values)) }
+
+        return myMap
     }
 }
